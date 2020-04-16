33d08ea598d52a1368703cd06e2b1b7002b86ebd
Melvin 760p <mhmtsui@connect.hku.hk>
2020-04-16 22:09:36 +0800

    <feat> initial commit

--- Network.h
+++ Network.h
@@ -1,10 +1,10 @@
 // Adjust the following values to match your needs
 // -----------------------------------------------
 #define   servername "fileserver"  // Set your server's logical name here e.g. if 'myserver' then address is http://myserver.local/
-IPAddress local_IP(192, 168, 0, 150); // Set your server's fixed IP address here
-IPAddress gateway(192, 168, 0, 1);    // Set your network Gateway usually your Router base address
+IPAddress local_IP(192, 168, 2, 88); // Set your server's fixed IP address here
+IPAddress gateway(192, 168, 2, 1);    // Set your network Gateway usually your Router base address
 IPAddress subnet(255, 255, 255, 0);   // Set your network sub-network mask here
-IPAddress dns(192,168,0,1);           // Set your network DNS usually your Router base address
+IPAddress dns(192, 168, 2, 1);           // Set your network DNS usually your Router base address
 const char ssid_1[]     = "your_SSID1";
 const char password_1[] = "your_PASSWORD_for SSID1";
 
